d4687bbdb20e9c9878e191eb98bfeb898e95ec7a
Jacob Keen <jaclkeen@gmail.com>
2016-09-16 14:00:05 -0500

    added editor rank filter

--- app/controllers/EditorRanksCtrl.js
+++ app/controllers/EditorRanksCtrl.js
@@ -2,14 +2,21 @@
 
 app.controller('EditorRanksCtrl', function($scope, ApiFactory){
 
+  $scope.position = "QB"
   $scope.editorRanks = []
 
-  ApiFactory.getEditorWeekRanks('QB')
-  .then(function(ranks){
-    let playerRanks = ranks.players
-    for(var key in playerRanks){
+  $scope.getERanks = function(position){
+    ApiFactory.getEditorWeekRanks(position)
+    .then(function(ranks){
+      console.log(ranks)
+      let playerRanks = ranks.players
+      $scope.editorRanks = []
+      for(var key in playerRanks){
         $scope.editorRanks.push(playerRanks[key])
-    }
-  })
+      }
+    })
+  }
+
+  $scope.getERanks("QB")
 
 })
